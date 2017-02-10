047ead34744e548e3a20a32d4b59d5844d207f04
upkanet <upkanet@gmail.com>
2017-02-10 11:59:01 +0100

    Adding User and Company Seeds

---
+++ database/seeds/CompaniesTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CompaniesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('companies')->insert([
+        	'user_id' => 1,
+        	'name' => 'World Company',
+        	'siret' => '123456789ABCDE',
+    	]);
+    }
+}
